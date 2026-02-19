5b90328426957a398307c01cdd2ce519fc4a7df9
ConstanceGosselin <gosselinconstance@outlook.com>
2026-02-19 20:19:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-// Smooth scroll pour les liens
-document.querySelectorAll('nav a').forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const target = document.querySelector(link.getAttribute('href'));
-    target.scrollIntoView({ behavior: 'smooth' });
-  });
-});
+// Si tu veux ajouter un menu mobile ou des animations + tard
+console.log("Mon portfolio chargé !");
