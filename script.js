ed31f3b804aed5cdf23e4b26d6f69e6bb6fb5c9e
ConstanceGosselin <gosselinconstance@outlook.com>
2026-02-24 10:54:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // Si tu veux ajouter un menu mobile ou des animations + tard
 console.log("Mon portfolio chargé !");
+const slides = document.querySelectorAll('.split-slider .slide');
+let current = 0;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.toggle('active', i === index);
+  });
+}
+
+showSlide(current);
+
+setInterval(() => {
+  current = (current + 1) % slides.length;
+  showSlide(current);
+}, 3000); // défilement toutes les 3 secondes
